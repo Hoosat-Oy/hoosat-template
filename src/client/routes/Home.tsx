17505141c5a0b4fa7aca03998c377db9c8209cbb
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-05-12 18:41:08 +0300

    try to add helmet

--- src/client/routes/Home.tsx
+++ src/client/routes/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -6,6 +7,10 @@ export const Home = () => {
   const navigate = useNavigate();
   return (
     <>
+      <Helmet>
+          <title>Home</title>
+          <meta name="description" content="Home of Hoosat SSR template" />
+      </Helmet>
       <h1>Home</h1>
       <button onClick={() => { navigate('/about'); }}>Navigate to about</button>
     </>
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { renderToPipeableStream } from 'react-dom/server';
 import App from '../client/App';
 import { StaticRouter } from 'react-router-dom/server';
+import { Helmet } from "react-helmet";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,22 +13,25 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req: Request, res: Response) => {
-  const location = req.url;
+  const helmet = Helmet.renderStatic();
   const { pipe } = renderToPipeableStream(
-    <html>
-      <head>
-      </head>
-      <body>
-        <div id="root">
-          <React.StrictMode>
-            <StaticRouter location={req.url}>
-              <App />
-            </StaticRouter>
-          </React.StrictMode>
-        </div>
-        <script type="module" src="bundle.js"></script>
-      </body>
-    </html>
+      <html { ...helmet.htmlAttributes.toComponent() }>
+        <head>
+          { helmet.title.toComponent() }
+          { helmet.meta.toComponent() }
+          { helmet.link.toComponent() }
+        </head>
+        <body { ...helmet.bodyAttributes.toComponent() }>
+          <div id="root">
+            <React.StrictMode>
+              <StaticRouter location={req.url}>
+                <App />
+              </StaticRouter>
+            </React.StrictMode>
+          </div>
+          <script src="bundle.js"></script>
+        </body>
+      </html>
   );
   res.setHeader('content-type', 'text/html');
   pipe(res);
